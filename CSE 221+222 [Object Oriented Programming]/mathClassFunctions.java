bb2cfd173d6ee3780945b3bdaea7f01e55d7c046
Mohammad Sohaib Islam Shibly <86130609+ShiblySohaib@users.noreply.github.com>
2022-09-11 12:56:44 +0600

    Create mathClassFunctions.java

---
+++ CSE 221+222 [Object Oriented Programming]/mathClassFunctions.java
@@ -0,0 +1,16 @@
+public class test {
+    public static void main(String[]args)
+    {
+        System.out.println(Math.abs(-40.2));
+        System.out.println(Math.sqrt(16));
+        System.out.println(Math.pow(3,3));
+        System.out.println(Math.PI);
+        System.out.println(Math.min(4,2));
+        System.out.println(Math.max(4,2));
+        System.out.println(Math.floor(2.6));
+        System.out.println(Math.ceil(-2.1));
+        System.out.println(Math.exp(1));
+        System.out.println(Math.round(2.5));
+        System.out.println(Math.random());
+    }
+}
