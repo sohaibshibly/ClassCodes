ebcd1a42f33ffe626d46cc06d7e1ba1b5fbb3456
Mohammad Sohaib Islam Shibly <86130609+ShiblySohaib@users.noreply.github.com>
2022-09-11 12:39:19 +0600

    Create scanDifferentTypes.java

---
+++ CSE 221+222 [Object Oriented Programming]/ScannerClass/scanDifferentTypes.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class AddTwoInt {
+    public static void main(String[]args)
+    {
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter name: ");
+        String a= input.next();
+        System.out.println("Enter age: ");
+        int b= input.nextInt();
+        System.out.println("Enter cgpa: ");
+        double c= input.nextDouble();
+        System.out.println("Enter dept: ");
+        String d= input.nextLine();
+        System.out.println("Name: "+a);
+        System.out.println("age: "+b);
+        System.out.println("cgpa: "+c);
+        System.out.println("dept: "+d);
+
+    }
+}
