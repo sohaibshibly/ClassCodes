7a70363760e042e80ada57d23471b4ff4574bfde
Mohammad Sohaib Islam Shibly <86130609+ShiblySohaib@users.noreply.github.com>
2022-09-11 13:15:39 +0600

    Create test.java

---
+++ CSE 221+222 [Object Oriented Programming]/javaArray/test.java
@@ -0,0 +1,10 @@
+public class test {
+    public static void main(String[]args)
+    {
+        int []arr={10,20,30,40,50};
+        for (int i : arr) //for(i=0;i<arr.length;i++)
+        {
+            System.out.println("The value of array: " + i);
+        }
+    }
+}
