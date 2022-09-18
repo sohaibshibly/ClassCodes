b6c1f1effc85cb161850f9ff47c2dd30dda1c590
Mohammad Sohaib Islam Shibly <86130609+ShiblySohaib@users.noreply.github.com>
2022-09-18 13:08:11 +0600

    Update Box.java

--- CSE 221+222 [Object Oriented Programming]/constructor overload/Box.java
+++ CSE 221+222 [Object Oriented Programming]/constructor overload/Box.java
@@ -14,9 +14,9 @@ public class Box {
         Box b3=new Box(50,60);
         Box b4=new Box();
 
-        System.out.println("For Box-1: Height = "+b1.height+" Width = "+b1.width);
-        System.out.println("For Box-2: Height = "+b2.height+" Width = "+b2.width);
-        System.out.println("For Box-3: Height = "+b3.height+" Width = "+b3.width);
-        System.out.println("For Box-4: Height = "+b4.height+" Width = "+b4.width);
+        System.out.println("For Box-1: Height = "+b1.height+"\tWidth = "+b1.width);
+        System.out.println("For Box-2: Height = "+b2.height+"\tWidth = "+b2.width);
+        System.out.println("For Box-3: Height = "+b3.height+"\tWidth = "+b3.width);
+        System.out.println("For Box-4: Height = "+b4.height+"\tWidth = "+b4.width);
     }
 }
